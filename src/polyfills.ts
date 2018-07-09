dce48341db28456f0e8dd573457584e4e90414cd
Marcos Bergamo <marcos@thedon.com.br>
2018-07-09 19:43:44 +0200

    initial commit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -71,6 +71,8 @@ import 'core-js/es7/reflect';
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+
+
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
